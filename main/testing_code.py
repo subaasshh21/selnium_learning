38f402c81e4d253823ccd9f7348f90aa0281803b
ramprakash <ram.prakash@dfinsolutions.com>
2025-06-21 06:58:24 +0530

    conflicts resolved again

--- main/testing_code.py
+++ main/testing_code.py
@@ -2,6 +2,10 @@ class Myclass:
     def my_method(self):
         print("hello")
 
+    def ram_method(self):
+        print("welcome to my world")
+        print("new changes")
+
     def my_method_2(self):
         print("welcome")# inprogress-> conflicts resolve
 
