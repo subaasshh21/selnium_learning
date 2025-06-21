5166e76bedc3e2491ccdce219f8d265b0e1e3b7c
subash <subashvenkatrao@gmail.com>
2025-06-20 21:02:25 -0400

    New code implementation Completed For My method 2

--- main/testing_code.py
+++ main/testing_code.py
@@ -2,5 +2,8 @@ class Myclass:
     def my_method(self):
         print("hello")
 
+    def my_method_2(self):
+        print("welcome")# inprogress-> conflicts resolve
+
 obj = Myclass()
 obj.my_method()
